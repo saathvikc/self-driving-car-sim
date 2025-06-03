5c109694cd1f335bbc5901341712263bef448fc3
saathvikc <83232359+saathvikc@users.noreply.github.com>
2025-06-03 15:01:58 -0400

    created road and added mechanisms
    Added road mechanics such as the lane and border divisions, and updated movement so car stays centered while road moves

--- controls.js
+++ controls.js
@@ -1,48 +1,48 @@
 class Controls {
-  constructor() {
-    this.forward = false;
-    this.left = false;
-    this.right = false;
-    this.reverse = false;
+    constructor() {
+        this.forward = false;
+        this.left = false;
+        this.right = false;
+        this.reverse = false;
 
-    this.#addKeyboardListeners();
-  }
+        this.#addKeyboardListeners();
+    }
 
-  #addKeyboardListeners() {
-    document.onkeydown = (event) => {
-      switch (event.key) {
-        case "ArrowLeft":
-          this.left = true;
-          break;
-        case "ArrowRight":
-          this.right = true;
-          break;
-        case "ArrowDown":
-          this.reverse = true;
-          break;
-        case "ArrowUp":
-          this.forward = true;
-          break;
-      }
-      //   console.table(this);
-    };
+    #addKeyboardListeners() {
+        document.onkeydown = (event) => {
+            switch (event.key) {
+                case "ArrowLeft":
+                    this.left = true;
+                    break;
+                case "ArrowRight":
+                    this.right = true;
+                    break;
+                case "ArrowDown":
+                    this.reverse = true;
+                    break;
+                case "ArrowUp":
+                    this.forward = true;
+                    break;
+            }
+            //   console.table(this);
+        };
 
-    document.onkeyup = (event) => {
-      switch (event.key) {
-        case "ArrowLeft":
-          this.left = false;
-          break;
-        case "ArrowRight":
-          this.right = false;
-          break;
-        case "ArrowDown":
-          this.reverse = false;
-          break;
-        case "ArrowUp":
-          this.forward = false;
-          break;
-      }
-      //   console.table(this);
-    };
-  }
+        document.onkeyup = (event) => {
+            switch (event.key) {
+                case "ArrowLeft":
+                    this.left = false;
+                    break;
+                case "ArrowRight":
+                    this.right = false;
+                    break;
+                case "ArrowDown":
+                    this.reverse = false;
+                    break;
+                case "ArrowUp":
+                    this.forward = false;
+                    break;
+            }
+            //   console.table(this);
+        };
+    }
 }
