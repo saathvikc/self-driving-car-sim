f936adc0e034f331a2405ab60d1b0430f7556133
saathvikc <83232359+saathvikc@users.noreply.github.com>
2025-06-04 11:42:52 -0400

    Added sensor class with update and draw functions (basic functionality)

--- car.js
+++ car.js
@@ -11,11 +11,13 @@ class Car {
         this.friction = 0.05;
         this.angle = 0;
 
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
     update() {
         this.#move();
+        this.sensor.update();
     }
 
     #move() {
@@ -65,9 +67,12 @@ class Car {
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
         ctx.fill();
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
