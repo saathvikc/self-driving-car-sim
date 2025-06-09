e863bc1f3912b0495876fa6251633f4eb98c94dc
saathvikc <83232359+saathvikc@users.noreply.github.com>
2025-06-09 14:10:09 -0400

    added basic neural network class and visualizer

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
-const canvas = document.getElementById("myCanvas");
-canvas.width = 200;
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 200;
 
-const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const newtorkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 300;
+
+const carCtx = carCanvas.getContext("2d");
+const newtworkCtx = newtorkCanvas.getContext("2d");
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)]
 
 animate();
 
-function animate() {
+function animate(time) {
     for (let i = 0; i < traffic.length; i++) {
         traffic[i].update(road.borders, []);
     }
     car.update(road.borders, traffic);
-    canvas.height = window.innerHeight;
+    carCanvas.height = window.innerHeight;
+    newtorkCanvas.height = window.innerHeight;
 
-    ctx.save();
-    ctx.translate(0, -car.y + canvas.height*0.75);
+    carCtx.save();
+    carCtx.translate(0, -car.y + carCanvas.height*0.75);
 
-    road.draw(ctx);
+    road.draw(carCtx);
 
     for (let i = 0; i < traffic.length; i++) {
-        traffic[i].draw(ctx, "green");
+        traffic[i].draw(carCtx, "green");
     }
-    car.draw(ctx, "blue");
+    car.draw(carCtx, "blue");
 
-    ctx.restore();
+    carCtx.restore();
+
+    newtworkCtx.lineDashOffset = -time/50;
+    Visualizer.drawNetwork(newtworkCtx, car.brain);
     requestAnimationFrame(animate);
 }
--- utils.js
+++ utils.js
@@ -41,3 +41,12 @@ function polysIntersect(p1, p2) {
 
     return false;
 }
+
+function getRGBA(value){
+    const alpha=Math.abs(value);
+    const R=value<0?0:255;
+    const G=R;
+    const B=value>0?0:255;
+    return "rgba("+R+","+G+","+B+","+alpha+")";
+}
+       
